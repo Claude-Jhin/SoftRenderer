16e3450756bcc7776f1fbd6614a60556aa2f262b
Claude-Jhin <76059275+Claude-Jhin@users.noreply.github.com>
2023-03-02 23:30:51 +0800

    header construct

--- tgaimage.cpp
+++ tgaimage.cpp
@@ -1,2 +1,118 @@
-﻿
-#include "tgaimage.h"
+﻿#include "tgaimage.h"
+
+#include <iostream>
+
+TGAImage::TGAImage()
+    : data(nullptr),
+      width(0),
+      height(0),
+      bytespp(0)
+{
+}
+
+TGAImage::TGAImage(int w, int h, int bpp)
+    : data(nullptr),
+      width(w),
+      height(h),
+      bytespp(bpp)
+{
+    unsigned long nbytes = width * height * bytespp;
+    data = new unsigned char[nbytes];
+    memset(data, 0, nbytes);
+}
+
+TGAImage::TGAImage(const TGAImage& other)
+{
+    width = other.width;
+    height = other.height;
+    bytespp = other.bytespp;
+    unsigned long nbytes = width * height * bytespp;
+    data = new unsigned char[nbytes];
+    memcpy(data, other.data, nbytes);
+}
+
+TGAImage::~TGAImage()
+{
+    delete[] data;
+}
+
+TGAImage& TGAImage::operator=(const TGAImage& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    width = other.width;
+    height = other.height;
+    bytespp = other.bytespp;
+    unsigned long nbytes = width * height * bytespp;
+    data = new unsigned char[nbytes];
+    memcpy(data, other.data, nbytes);
+
+    return *this;
+}
+
+bool TGAImage::read_tga_file(const char* filename)
+{
+    delete[] data;
+    data = nullptr;
+
+    ifstream in;
+    in.open(filename, ios::binary);
+    if (!in.is_open())
+    {
+        cerr << "can't open file " << filename << "\n";
+        in.close();
+        return false;
+    }
+
+    TGA_Header header;
+    in.read((char*)&header, sizeof(header));
+
+    return true;
+}
+
+TGAColor TGAImage::get(int x, int y)
+{
+    if (!data || x < 0 || y < 0 || x >= width || y >= height)
+    {
+        return TGAColor();
+    }
+    return TGAColor(data + (x + y * width) * bytespp, bytespp);
+}
+
+bool TGAImage::set(int x, int y, TGAColor c)
+{
+    if (!data || x < 0 || y < 0 || x >= width || y >= height)
+    {
+        return false;
+    }
+    memcpy(data + (x + y * width) * bytespp, c.raw, bytespp);
+    return true;
+}
+
+int TGAImage::get_width()
+{
+    return width;
+}
+
+int TGAImage::get_height()
+{
+    return height;
+}
+
+int TGAImage::get_bytespp()
+{
+    return bytespp;
+}
+
+unsigned char* TGAImage::buffer()
+{
+    return data;
+}
+
+void TGAImage::clear()
+{
+    memset((void*)data, 0, width * height * bytespp);
+}
--- tgaimage.h
+++ tgaimage.h
@@ -1,15 +1,139 @@
-﻿
+﻿#include <fstream>
+using namespace std;
+
+#pragma pack(push,1)
 struct TGA_Header
 {
+    char idlength;
+    char colormaptype;
+    char datatypecode;
+    short colormaporigin;
+    short colormaplength;
+    char colormapdepth;
+    short x_origin;
+    short y_origin;
+    short width;
+    short height;
+    char bitsperpixel;
+    char imagedescriptor;
     
 };
+#pragma pack(pop)
 
-struct TGAColor {
-   
+struct TGAColor
+{
+    TGAColor(): val(0), bytespp(1)
+    {
+    }
+
+    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A)
+        : b(B),
+          g(G),
+          r(R),
+          a(A),
+          bytespp(4)
+    {
+    }
+
+    TGAColor(unsigned v, int bpp)
+        : val(v),
+          bytespp(bpp)
+    {
+    }
+
+    TGAColor(const TGAColor& other)
+        : val(other.val),
+          bytespp(other.bytespp)
+    {
+    }
+
+    TGAColor(const unsigned char* p, int bpp)
+        : val(0),
+          bytespp(bpp)
+    {
+        for (int i = 0; i < bpp; ++i)
+        {
+            raw[i] = p[i];
+        }
+    }
+
+
+    TGAColor& operator=(const TGAColor& other)
+    {
+        if (this == &other)
+            return *this;
+        val = other.val;
+        bytespp = other.bytespp;
+        return *this;
+    }
+
+
+    union
+    {
+        struct
+        {
+            unsigned char b, g, r, a;
+        };
+
+        unsigned char raw[4];
+        unsigned int val;
+    };
+
+    int bytespp;
 };
 
 
 class TGAImage
 {
+public:
+    enum Format
+    {
+        GRAYSCALE = 1,
+        RGB = 3,
+        RGBA = 4
+    };
 
+    TGAImage();
+
+    TGAImage(int w, int h, int bpp);
+
+    TGAImage(const TGAImage& other);
+
+    ~TGAImage();
+
+    TGAImage& operator=(const TGAImage& other);
+
+    bool read_tga_file(const char* filename);
+
+    bool write_tga_file(const char* filename);
+
+    bool flip_horizontally();
+    
+    bool flip_vertically();
+
+    bool scale(int w, int h);
+
+    TGAColor get(int x, int y);
+
+    bool set(int x, int y, TGAColor c);
+
+    int get_width();
+
+    int get_height();
+
+    int get_bytespp();
+
+    unsigned char* buffer();
+
+    void clear();
+
+protected:
+
+    bool load_rle_data(std::ifstream &in);
+    bool unload_rle_data(std::ofstream &out);
+    
+    unsigned char* data;
+    int width;
+    int height;
+    int bytespp;
 };
