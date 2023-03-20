86a286d5a2c4ec2fffeaacd9a9b687c1892b713c
Claude-Jhin <76059275+Claude-Jhin@users.noreply.github.com>
2023-03-20 13:58:00 +0800

    line render

---
+++ model.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <vector>
+#include "geometry.h"
+
+class Model
+{
+private:
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<int>> faces_;
+public:
+    Model(const char* filename);
+    ~Model();
+
+    int nverts();
+    int nfaces();
+    Vec3f vert(int idx);
+    std::vector<int> face(int idx);
+};
